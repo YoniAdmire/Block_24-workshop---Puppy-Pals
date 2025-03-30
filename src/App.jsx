dd2fd315b8a80681132688bc767ae073fc98ca23
YoniAdmire <taibyonas@gmail.com>
2025-03-30 18:06:20 -0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import {puppyList} from './data.js'
+import './App.css'
+
+function App() {
+  const [puppies, setPuppies] = useState(puppyList)
+  const [featPupId, setFeatPupId] = useState(null);
+
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+
+  return (
+    
+    <div className="App">
+      <h1>Puppy List</h1>
+      
+      {puppies.map((puppy) => {
+        return (
+          <p 
+            onClick={() => setFeatPupId(puppy.id)} 
+            key={puppy.id}
+            className="puppy-name"
+          >
+            {puppy.name}
+          </p>
+        );
+      })}
+
+      {featPupId && (
+        <div className="featured-puppy">
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+    </div>
+    
+  );
+}
+
+export default App
